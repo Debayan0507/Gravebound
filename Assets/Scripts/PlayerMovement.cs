04964d129015e380b461048452925e94f38385d6
Debayan Mukherjee <debayanm054@gmail.com>
2026-07-29 23:24:11 +0530

    Implemented Player Move and Jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D playerRb;
+    public float horizontalInput;
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    public bool isGrounded = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Move();
+    }
+    void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        {
+            playerRb.AddForceY(jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+    void Move()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        playerRb.linearVelocity = new Vector2(horizontalInput * moveSpeed, playerRb.linearVelocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
